5512cee43f942fd0eaddf60aaa752c4799c957fa
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-16 01:17:23 +0300

    Add saveItem method with JSON encoder and decoder

--- MVVM-SwiftUI-ToDoList/ViewModels/ListViewModel.swift
+++ MVVM-SwiftUI-ToDoList/ViewModels/ListViewModel.swift
@@ -8,19 +8,26 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "First title", isCompleted: false),
-            ItemModel(title: "Second title", isCompleted: true)
-        ]
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
         
-        items.append(contentsOf: newItems)
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -39,6 +46,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try?  JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
     
