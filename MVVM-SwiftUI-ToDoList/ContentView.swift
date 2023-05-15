62c79d8c6a6fa2450de5676e9730da1854209dcd
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-15 15:42:28 +0300

    Add swiftUI project

---
+++ MVVM-SwiftUI-ToDoList/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MVVM-SwiftUI-ToDoList
+//
+//  Created by Çağrı Tuğberk Masat on 15.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM-SwiftUI-ToDoList/MVVM_SwiftUI_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUI_ToDoListApp.swift
+//  MVVM-SwiftUI-ToDoList
+//
+//  Created by Çağrı Tuğberk Masat on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUI_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
