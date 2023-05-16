c22e88ba658dad98d66e4afa62dbab1ba5c8edd2
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-16 23:35:36 +0300

    Add todo list for the new updates

--- MVVM-SwiftUI-ToDoList/Models/ItemModel.swift
+++ MVVM-SwiftUI-ToDoList/Models/ItemModel.swift
@@ -22,3 +22,12 @@ struct ItemModel: Identifiable, Codable {
         ItemModel(id: id, title: title, isCompleted: !isCompleted)
     }
 }
+
+/*
+ TODOs
+ 1) Update textfield to be able to write more than 1 line
+ 2) Add title and priorty and date for TODO cells
+ 3) Add combine
+ 4) Update folder structure like -> CustomViews, Pages, Models, ViewModels and Views
+ 5) Add viewModelPresentation
+ */
