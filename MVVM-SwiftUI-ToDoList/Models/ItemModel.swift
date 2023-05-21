99e957823549c1b22ac0312fbfb7e0dc09355117
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-21 23:50:17 +0300

    Remove commented lines

--- MVVM-SwiftUI-ToDoList/Models/ItemModel.swift
+++ MVVM-SwiftUI-ToDoList/Models/ItemModel.swift
@@ -32,12 +32,3 @@ struct ItemModel: Identifiable, Codable {
         ItemModel(id: id, title: title, description: description, priority: priority, dueDate: dueDate, isCompleted: isCompleted)
     }
 }
-
-/*
- TODOs
- 1) Update textfield to be able to write more than 1 line ---- OK
- 2) Add title and priorty and date for TODO cells
- 3) Add combine
- 4) Update folder structure like -> CustomViews, Pages, Models, ViewModels and Views
- 5) Add viewModelPresentation
- */
